b14cfa45851236c1f46420aa1e9bf1f45eccda55
JoseIgnacioTapia <josignaciot@gmail.com>
2021-10-07 18:19:26 -0300

    Using the useEffect Hook for animation button

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import classes from './HeaderCardButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import { CartContext } from '../../context/CartContext';
@@ -6,13 +6,36 @@ import { CartContext } from '../../context/CartContext';
 const HeaderCardButton = props => {
   const cartCtx = useContext(CartContext);
 
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+
+  const { items } = cartCtx;
+
   // Stores the number of items per item type:
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false); // For deleting the animation button class
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClickShow}>
+    <button className={btnClasses} onClick={props.onClickShow}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
