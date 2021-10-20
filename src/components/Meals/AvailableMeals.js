5b7ccb9fdf635a186113cd61f6a431d8a30b7c74
JoseIgnacioTapia <josignaciot@gmail.com>
2021-10-20 16:58:05 -0300

    Fetching Meals via Http

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,45 +1,48 @@
+import { useState, useEffect } from 'react';
+
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
-const mealList = DUMMY_MEALS.map(meal => (
-  <MealItem
-    id={meal.id} // Fixing label problem
-    key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-  />
-));
-
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://food-order-app-96653-default-rtdb.firebaseio.com/meals.json'
+      );
+      const data = await response.json();
+      console.log(data);
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      console.log(meals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealList = meals.map(meal => (
+    <MealItem
+      id={meal.id} // Fixing label problem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
     <section className={classes.meals}>
       <Card>
