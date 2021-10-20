b91a8b707039ba2a04761cf6206d5324980c06ed
JoseIgnacioTapia <josignaciot@gmail.com>
2021-10-20 17:50:06 -0300

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,12 +6,19 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://food-order-app-96653-default-rtdb.firebaseio.com/meals.json'
+        'https://food-order-app-96653-default-rtdb.firebaseio.com/meals'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const data = await response.json();
       console.log(data);
 
@@ -28,10 +35,31 @@ const AvailableMeals = () => {
 
       setMeals(loadedMeals);
       console.log(meals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    // Handling an error inside of a promise
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={classes['loading-text']}>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes['error-text']}>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map(meal => (
     <MealItem
